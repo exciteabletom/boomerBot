26f984aca0bf0c74f4e7728c9e4aa3f09c9446ba
exciteabletom <tom@digitalnook.net>
2020-10-14 12:04:05 +0800

    list -> tuple

--- main.py
+++ main.py
@@ -5,23 +5,27 @@ from private import prodToken, devToken
 
 import time
 
-boomerWords = ["millennial", "liberal", "milennial", "these days", "kids these days", "back in","in my day", "when i was young", "we used to", "we never", "snowflake", "lazy",
-               "entitled", "use our imagination", "damn kids", "out of hand", "gone too far", "gone to far", "the war", "the depression", "aoc", "trump", "stupid kids",
-               "stop complaining", "republican", "trump", "bernie", "biden", "clinton", "conservative", "liberal" "politics", "out of hand",":))", ":((", "@okboomer_bot",
-		"get over it", "it's not that bad", "its not that bad", "maga", "pence"]
+boomerWords = (
+	"millennial", "liberal", "milennial", "these days", "kids these days", "back in", "in my day",
+	"when i was young", "we used to", "we never", "snowflake", "lazy", "entitled", "use our imagination",
+	"damn kids", "out of hand", "gone too far", "gone to far", "the war", "the depression", "aoc", "trump",
+	"stupid kids", "stop complaining", "republican", "trump", "bernie", "biden", "clinton", "conservative",
+	"liberal" "politics", "out of hand", ":))", ":((", "@okboomer_bot", "get over it", "it's not that bad",
+	"its not that bad", "maga", "pence",
+)
+
 
 def main_loop(msg):
-    contentType, chatType, chatId = telepot.glance(msg)
+	contentType, chatType, chatId = telepot.glance(msg)
 
-    if contentType == "text":
-        msgText = msg["text"].lower()  # message sent converted to lower case
+	if contentType == "text":
+		msgText = msg["text"].lower()  # message sent converted to lower case
 
-        for word in boomerWords:  # loop through array of boomer words
-            if word in msgText:  # if a boomer word is in the message
-                message = f"Ok, Boomer.\n\nTriggered by the phrase '{word}'" 
-                bot.sendMessage(chatId, message)  # send epic ok boomer roast
-                break  # if roast is sent stop loop
-
+		for word in boomerWords:  # loop through array of boomer words
+			if word in msgText:  # if a boomer word is in the message
+				message = f"Ok, Boomer.\n\nTriggered by the phrase '{word}'"
+				bot.sendMessage(chatId, message)  # send epic ok boomer roast
+				break  # if roast is sent stop loop
 
 
 bot = telepot.Bot(prodToken)
@@ -29,4 +33,4 @@ bot = telepot.Bot(prodToken)
 MessageLoop(bot, main_loop).run_as_thread()  # constantly checks for new messages, if new message call main_loop()
 
 while True:  # keeps program alive
-    time.sleep(10)
+	time.sleep(10)
